f7d87a2569cd4584c836ee5fc255d22f5b6267d1
Foad Ardalan <foad.ardalan@outlook.fr>
2018-04-29 18:26:12 +0200

    renameing

---
+++ Angular/libs/authentication/src/+state/selectors/authentication-state.selectors.ts
@@ -0,0 +1,16 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { StatusState } from '../states/status.state';
+import { LoginPageState } from '../states/login-page.state';
+import { AuthenticationState } from '../states/authentication-state.state';
+
+const selectAuthenticationState = createFeatureSelector<AuthenticationState>('authentication');
+const selectAuthenticationStatusState = createSelector(
+  selectAuthenticationState,
+  (state: AuthenticationState) => state.status
+);
+const selectLoginPageState = createSelector(selectAuthenticationState, (state: AuthenticationState) => state.loginPage);
+
+export const getLoggedIn = createSelector(selectAuthenticationStatusState, (state: StatusState) => !!state.user);
+export const getUser = createSelector(selectAuthenticationStatusState, (state: StatusState) => state.user);
+export const getLoginPageError = createSelector(selectLoginPageState, (state: LoginPageState) => state.error);
+export const getLoginPagePending = createSelector(selectLoginPageState, (state: LoginPageState) => state.pending);
