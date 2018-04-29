187e9f8ecda3305fb4b2863f0411ce7c7bf92760
Foad Ardalan <foad.ardalan@outlook.fr>
2018-04-29 18:52:00 +0200

    renaming

--- Angular/libs/user/src/components/delete/user-delete.dialog.component.ts
+++
@@ -1,9 +0,0 @@
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Component, Inject } from '@angular/core';
-
-@Component({
-  templateUrl: './user-delete.dialog.component.html'
-})
-export class UserDeleteDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
-}
--- angular/libs/task/src/components/delete/task-delete.dialog.component.ts
+++
@@ -1,9 +0,0 @@
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Component, Inject } from '@angular/core';
-
-@Component({
-  templateUrl: './task-delete.dialog.component.html'
-})
-export class TaskDeleteDialogComponent {
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
-}
